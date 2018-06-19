7e6b95f75a46659dfd6fa37367752f81c00a170a
Ohad Duering <ohad.duering@sap.com>
2018-06-19 14:35:33 +0200

    add login page and logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
 	<div>
 		<h1>Expensify</h1>
 		<NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink><br />
 		<NavLink to="/create" activeClassName="is-active">Create Expense</NavLink><br />
 		<NavLink to="/help" activeClassName="is-active">Help</NavLink>
+		<button onClick={startLogout}>Logout</button>
 	</div>
 );
 
-export default Header;
+const mapDispathToProps = (dispatch) => ({
+	startLogout: () => dispatch(startLogout())
+});
+
+export default connect(undefined, mapDispathToProps)(Header);

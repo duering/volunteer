92ad2ad59fcf3d5414f8177f253c0a3bb8f66d4b
Ohad Duering <ohad.duering@sap.com>
2018-06-19 11:25:12 +0200

    Add startEditExpense

--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -1,7 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, 
-	startSetExpenses, startRemoveExpense } from '../../actions/expenses'
+	startSetExpenses, startRemoveExpense, startEditExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -31,6 +31,31 @@ test('should setup edit expense action object', () => {
 		updates: {
 			note: 'test'
 		}
+	});
+});
+
+test('should edit expenses from firebase', (done) => {
+	const store = createMockStore({});
+	const id = expenses[1].id;
+	const updates = {
+		note: 'new note'
+	};
+
+	store.dispatch(startEditExpense(id, updates)).then(() => {
+		
+		const actions = store.getActions();
+		expect(actions.length).toBe(1);
+		
+		expect(actions[0]).toEqual({
+			type: 'EDIT_EXPENSE',
+			id,
+			updates
+		});
+
+		return database.ref(`expenses/${id}/note`).once('value');
+	}).then((snapshot) => {
+		expect(snapshot.val()).toBe('new note');
+		done();
 	});
 });
 

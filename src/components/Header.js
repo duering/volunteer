58da6a0a07fc3aa793a80c030dd94a047978b878
ohad duering <ohad.duering@sap.com>
2018-06-21 16:32:10 +0200

    Store user data in user area

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ import { startLogout } from '../actions/auth';
 export const Header = ({ startLogout }) => (
 	<div>
 		<h1>Expensify</h1>
-		<NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink><br />
+		<NavLink to="/dashboard" activeClassName="is-active" exact={true}>Dashboard</NavLink><br />
 		<NavLink to="/create" activeClassName="is-active">Create Expense</NavLink><br />
-		<NavLink to="/help" activeClassName="is-active">Help</NavLink>
 		<button onClick={startLogout}>Logout</button>
 	</div>
 );
